7fe52cbecc892cac6553d940bf21f4882309f0a1
Ilya <112574854+skrimza@users.noreply.github.com>
2024-10-11 15:41:10 +0300

    change docker-compose

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from datetime import timedelta
 from flask import Flask
 from sessions.session import generic_session_key
 
-
 app = Flask(__name__)
-
-
-
 
 app.secret_key = generic_session_key()
 app.permanent_session_lifetime = timedelta(days=1)
